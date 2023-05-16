dbe6ad15bd0c9796248c44c11e06532ea1ab8ff3
krisz3001 <horvath.krisztian3001@gmail.com>
2023-05-16 21:55:50 +0200

    high contrast mode added, description page reworked

---
+++ js/contrast.js
@@ -0,0 +1,6 @@
+var css = document.getElementById('contrast');
+css.disabled = JSON.parse(localStorage.getItem('contrast'))
+function toggleContrast(){
+    css.disabled = !css.disabled;
+    localStorage.setItem("contrast", css.disabled);
+}
