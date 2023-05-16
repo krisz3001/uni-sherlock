9ee58e8bec19e1cd85184be2ac244fdedd82830b
krisz3001 <horvath.krisztian3001@gmail.com>
2023-05-16 22:04:46 +0200

    fix contrast mode

--- js/contrast.js
+++ js/contrast.js
@@ -1,6 +1,7 @@
 var css = document.getElementById('contrast');
-css.disabled = JSON.parse(localStorage.getItem('contrast'))
+css.disabled = !JSON.parse(localStorage.getItem('contrast'))
 function toggleContrast(){
     css.disabled = !css.disabled;
-    localStorage.setItem("contrast", css.disabled);
+    localStorage.setItem("contrast", !css.disabled);
 }
+console.log(localStorage);
